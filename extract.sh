2e8e4dbb905bcbd0cbc61390de5650f270623966
Bert Spaan <bert.spaan@gmail.com>
2019-03-20 14:48:34 +0100

    new colors + land/water in viz

--- extract.sh
+++ extract.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
-# # MBUtil is an old Mapbox tool to extract tiles from mbtiles databases:
-# #   https://github.com/mapbox/mbutil
-# # (There might be newer and better solutions, but I couldn't find one)
-# # However, it works, and people seem to still use MBUtil:
-# #   https://github.com/klokantech/vector-tiles-sample#host-the-vector-tiles-without-any-server-at-all
-# mb-util --image_format=pbf sequences.mbtiles tiles
+# Convert to GeoJSON:
+./ndjson-to-geojson.js < sequences.ndjson > sequences.geojson
 
-# # The extracted files are gzipped, the following commands unzips them:
-# find ./tiles -name "*.pbf" -exec mv {} {}.gz \;
-# gunzip ./tiles -r
+# Compute stats:
+./stats.js < sequences.ndjson > stats.json
+
+tippecanoe --force -z14 -o sequences.mbtiles \
+  --exclude=coordinateProperties \
+   sequences.geojson
+
+rm -rf tiles
+
+# MBUtil is an old Mapbox tool to extract tiles from mbtiles databases:
+#   https://github.com/mapbox/mbutil
+# (There might be newer and better solutions, but I couldn't find one)
+# However, it works, and people seem to still use MBUtil:
+#   https://github.com/klokantech/vector-tiles-sample#host-the-vector-tiles-without-any-server-at-all
+mb-util --image_format=pbf sequences.mbtiles tiles
+
+# The extracted files are gzipped, the following commands unzips them:
+find ./tiles -name "*.pbf" -exec mv {} {}.gz \;
+gunzip ./tiles -r
 
 while read sequence; do
   filename="$(jq -r '.id' <<< $sequence).geojson"
@@ -20,12 +32,4 @@ while read sequence; do
 
   mkdir -p $path
   echo "$sequence" > $path/$filename
-
-
-
-  # dir=
-
-  # 2016-03-17
-#   echo $in
-#   echo "beer!!!"
 done < sequences.ndjson
